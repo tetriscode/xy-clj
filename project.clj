b3c6a1da72424de6cf8d86687240e1cdb94edc29
Wes Richardet <wes@tetriscodes.com>
2017-06-23 20:04:52 -0500

    Incrementing version to 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject xy "0.1.0"
+(defproject xy "0.2.0-SNAPSHOT"
   :description "XY: Idiomatic Clojure library for Geo"
   :url "http://github.com/tetriscode/xy"
   :license {:name "Eclipse Public License"
