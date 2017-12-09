8f4b85cd057c568736028a83705063429380c828
Wes Richardet <wes@tetriscodes.com>
2017-12-08 19:39:30 -0600

    Removing excessive files

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "http://github.com/tetriscode/xy"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-beta3"]
-                 [org.clojure/spec.alpha "0.1.134"]
+  :dependencies [[org.clojure/clojure "1.9.0-RC2"]
+                 [org.clojure/spec.alpha "0.1.143"]
                  [org.clojure/data.json "0.2.6"]
                  [org.locationtech.jts/jts-core "1.15.0-uhopper"
                   :exclusions [xerces/xercesImpl]]]
