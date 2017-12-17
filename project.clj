fa5b2c965c1fb472feb88f8b85f056c6ec81717e
Wes Richardet <wes@tetriscodes.com>
2017-12-16 22:03:02 -0600

    Bumping to Clojure version 1.9

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject tetriscode/xy "0.8.5"
+(defproject tetriscode/xy "0.8.6"
   :description "XY: A Clojure library for Geo"
   :url "http://github.com/tetriscode/xy"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-RC2"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/spec.alpha "0.1.143"]
                  [org.clojure/data.json "0.2.6"]
                  [org.locationtech.jts/jts-core "1.15.0-uhopper"
